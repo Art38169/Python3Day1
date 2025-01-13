eb81d69fea7a14c0ec865d0b6ed5f158c48fd2e5
Art38169 <00482@kvis.ac.th>
2025-01-13 12:15:32 +0700

    WTH is this?!?!

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-print("Hi")
-print("Hello")
+class Circle:
+    def __init__(self, radius, x, y):
+        self.radius = radius
+        self.x = x
+        self.y = y
+        
+    def draw(self):
+        print(f"{self.x}, {self.y}")
+    
+    def move(self, shift_x, shift_y):
+            self.x += shift_x
+            self.y += shift_y
+        
+        
+        
+        
+
+if __name__ == "__main__":
+    c = Circle(10, 100, 100)
+    
+    window_width = 300
+    window_height = 300
+    vx = 1
+    vy = 1
+    
+    for i in range(1000):
+        c.move(vx, vy)
+        c.draw()
+        if c.x > window_width or c.x < 0:
+            vx = -vx
+        if c.y > window_height or c.y < 0:
+            vy = -vy
+    
